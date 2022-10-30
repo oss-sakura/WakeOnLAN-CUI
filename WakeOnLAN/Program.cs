a49fe61160bcdcdfcd4aabad4815388b7fbdb645
IT SUPPORT SAKURA <just4fun@sakura-it.com>
2022-10-30 20:28:02 +0900

    1st commit

---
+++ WakeOnLAN/Program.cs
@@ -0,0 +1,79 @@
+﻿using WakeOnLAN;
+
+internal class Program
+{
+    /// <summary>
+    /// WakeOnLAN Main
+    /// </summary>
+    /// <param name="args">parameters</param>
+    /// <returns>last exit code</returns>
+    static int Main(string[] args)
+    {
+        if (!(args.Length == 2 || args.Length == 1))
+        {
+            Usage();
+            return -1;
+        }
+
+        Task<int>? task = null;
+
+        if (args.Length == 1)
+        {
+            task = WakeOnLAN(args[0]);
+        }
+        else
+        {
+            // args.Length == 2
+            if (int.TryParse(args[1], out int portNo))
+            {
+                task = WakeOnLAN(args[0], portNo);
+            }
+            else
+            {
+                Console.Error.WriteLine("Invalid port number.");
+            }
+        }
+
+        return task == null ? -1 : task.Result;
+    }
+
+    /// <summary>
+    /// WOL.WakeOnLan method call.
+    /// </summary>
+    /// <param name="macAddress"></param>
+    /// <returns>exit code</returns>
+    /// 
+
+    private static async Task<int> WakeOnLAN(string macAddress)
+    {
+        int result = await WOL.WakeOnLan(macAddress);
+        if (result != 0)
+        {
+            Console.Error.WriteLine(WOL.ExitCodeMessage);
+        }
+        return WOL.ExitCode;
+    }
+
+    private static async Task<int> WakeOnLAN(string macAddress, int portNo)
+    {
+        WOL.PortNo = portNo;
+        int result = await WOL.WakeOnLan(macAddress);
+        if (result != 0)
+        {
+            Console.Error.WriteLine(WOL.ExitCodeMessage);
+        }
+        return WOL.ExitCode;
+    }
+
+    /// <summary>
+    /// Output usage.
+    /// </summary>
+    private static void Usage()
+    {
+        Console.WriteLine("Usage: {0} mac-address [port]", System.AppDomain.CurrentDomain.FriendlyName);
+        Console.WriteLine("\te.g. {0} 12:34:56:AB:CD:EF", System.AppDomain.CurrentDomain.FriendlyName);
+        Console.WriteLine("\te.g. {0} 12-34-56-AB-CD-EF", System.AppDomain.CurrentDomain.FriendlyName);
+        Console.WriteLine("\te.g. {0} 12:34:56:AB:CD:EF 7", System.AppDomain.CurrentDomain.FriendlyName);
+        Console.WriteLine("\n\tdefault port is 9.");
+    }
+}
---
+++ WakeOnLAN/WOL.cs
@@ -0,0 +1,146 @@
+﻿using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace WakeOnLAN
+{
+    /// <summary>
+    /// Wake-on-LAN implementation class. 
+    /// WOL class used source code posted at the following URLs:
+    /// https://stackoverflow.com/questions/861873/wake-on-lan-using-c-sharp
+    /// https://stackoverflow.com/users/41745/sameet
+    /// https://stackoverflow.com/users/5741643/poul-bak
+    /// Thanks!😃
+    /// </summary>
+    public static class WOL
+    {
+        /// <summary>
+        /// Exit code for WOL class.
+        /// </summary>
+        private static int _ExitCode = 0;
+
+        /// <summary>
+        /// Wake-on-LAN magic packet port number.
+        /// </summary>
+        public static int PortNo { get; set; } = 9;
+
+        /// <summary>
+        /// Exception error message.
+        /// </summary>
+        private static string _ExitCodeMessage = "";
+
+        /// <summary>
+        /// Exit code get property.
+        /// </summary>
+        public static int ExitCode
+        {
+            get { return _ExitCode; }
+        }
+
+        /// <summary>
+        /// Exit code message get property.
+        /// </summary>
+        public static string ExitCodeMessage
+        {
+            get { return _ExitCodeMessage; }
+        }
+
+        /// <summary>
+        /// Do Wake-on-LAN.
+        /// </summary>
+        /// <param name="macAddress">MAC address</param>
+        /// <returns>result code. 0 is success.</returns>
+        public static async Task<int> WakeOnLan(string macAddress)
+        {
+            // Build WOL magic packet.
+            byte[] magicPacket;
+            try
+            {
+                magicPacket = BuildMagicPacket(macAddress);
+            }
+            catch (Exception ex)
+            {
+                _ExitCode = 1;
+                _ExitCodeMessage = ex.Message;
+                return ExitCode;
+            }
+
+            // Send magic packets by UDP.
+            try
+            {
+                if (!NetworkInterface.GetAllNetworkInterfaces().Where((n) =>
+                    n.NetworkInterfaceType != NetworkInterfaceType.Loopback && n.OperationalStatus == OperationalStatus.Up).Any())
+                {
+                    _ExitCode = 2;
+                    _ExitCodeMessage = "No valid network adapter found.";
+                    return ExitCode;
+                }
+
+                foreach (NetworkInterface networkInterface in NetworkInterface.GetAllNetworkInterfaces().Where((n) =>
+                    n.NetworkInterfaceType != NetworkInterfaceType.Loopback && n.OperationalStatus == OperationalStatus.Up))
+                {
+                    IPInterfaceProperties iPInterfaceProperties = networkInterface.GetIPProperties();
+                    foreach (MulticastIPAddressInformation multicastIPAddressInformation in iPInterfaceProperties.MulticastAddresses)
+                    {
+                        IPAddress multicastIpAddress = multicastIPAddressInformation.Address;
+                        if (multicastIpAddress.ToString().StartsWith("ff02::1%", StringComparison.OrdinalIgnoreCase)) // Ipv6: All hosts on LAN (with zone index)
+                        {
+                            UnicastIPAddressInformation? unicastIPAddressInformation = iPInterfaceProperties.UnicastAddresses.Where((u) =>
+                                u.Address.AddressFamily == AddressFamily.InterNetworkV6 && !u.Address.IsIPv6LinkLocal).FirstOrDefault();
+                            if (unicastIPAddressInformation != null)
+                            {
+                                await SendWakeOnLan(unicastIPAddressInformation.Address, multicastIpAddress, magicPacket);
+                            }
+                        }
+                        else if (multicastIpAddress.ToString().Equals("224.0.0.1")) // Ipv4: All hosts on LAN
+                        {
+                            UnicastIPAddressInformation? unicastIPAddressInformation = iPInterfaceProperties.UnicastAddresses.Where((u) =>
+                                u.Address.AddressFamily == AddressFamily.InterNetwork && !iPInterfaceProperties.GetIPv4Properties().IsAutomaticPrivateAddressingActive).FirstOrDefault();
+                            if (unicastIPAddressInformation != null)
+                            {
+                                await SendWakeOnLan(unicastIPAddressInformation.Address, multicastIpAddress, magicPacket);
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _ExitCode = 3;
+                _ExitCodeMessage = ex.Message;
+                return ExitCode;
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Building Wake On Lan Magic Packet.
+        /// </summary>
+        /// <param name="macAddress">MAC address</param>
+        /// <returns>Generated Magic Packet</returns>
+        static byte[] BuildMagicPacket(string macAddress) // MacAddress in any standard HEX format
+        {
+            macAddress = Regex.Replace(macAddress, "[: -]", "");
+            byte[] macBytes = Convert.FromHexString(macAddress);
+
+            IEnumerable<byte> header = Enumerable.Repeat((byte)0xff, 6); //First 6 times 0xff
+            IEnumerable<byte> data = Enumerable.Repeat(macBytes, 16).SelectMany(m => m); // then 16 times MacAddress
+            return header.Concat(data).ToArray();
+        }
+
+        /// <summary>
+        /// Send UDP Wake On Lan Magic Packet.
+        /// </summary>
+        /// <param name="localIpAddress"></param>
+        /// <param name="multicastIpAddress"></param>
+        /// <param name="magicPacket"></param>
+        /// <returns></returns>
+        static async Task SendWakeOnLan(IPAddress localIpAddress, IPAddress multicastIpAddress, byte[] magicPacket)
+        {
+            using UdpClient client = new(new IPEndPoint(localIpAddress, 0));
+            await client.SendAsync(magicPacket, magicPacket.Length, new IPEndPoint(multicastIpAddress, PortNo));
+        }
+    }
+}
